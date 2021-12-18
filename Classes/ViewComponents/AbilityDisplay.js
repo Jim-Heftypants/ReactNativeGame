669a508eaf353a6fd388584c376480f79c277660
Jim-Heftypants <nickcsercel@gmail.com>
2021-12-18 02:38:47 -0800

    stable ability buttons

--- Classes/ViewComponents/AbilityDisplay.js
+++ Classes/ViewComponents/AbilityDisplay.js
@@ -6,79 +6,96 @@ const circleDims = 100;
 class AbilityDisplay extends React.Component {
     constructor(props) {
         super(props);
-        this.abilities = {};
+        this.abilitiesObj = {};
         this.timer;
-        const charAbilities = this.props.Character.Attributes.Abilities;
+        // console.log("Abilities: " + JSON.stringify(this.props.Character.Data.Attributes.Abilities));
+        const charAbilities = this.props.Character.Data.Attributes.Abilities;
         const entities = Object.entries(charAbilities);
+        // console.log("entities: " + JSON.stringify(entities));
         for (let i = 0; i < entities.length; i++) {
-            this.abilities[entities[i][0]] = {
-                cooldown: entities[i][1],
+            this.abilitiesObj[entities[i][0]] = {
+                cooldown: entities[i][1].cooldown,
                 currentCooldown: 0,
                 baseColor: '255,0,0',
                 cooldownColor: '0,0,255',
-            }
+            // for (let j = 0; j < entities[i].length; j++) {
+                }
+            // }
         }
-        this.cooldowns = [];
+        this.abilitiesArr = Object.values(this.abilitiesObj);
+        // console.log("abilitiesArr: " + JSON.stringify(this.abilitiesArr));
     }
     createStyle(ability) {
+        // console.log("style ability: " + JSON.stringify(ability));
         const cooldown = ability.cooldown; // have to track seconds since cast manually
         const currentCooldown = ability.currentCooldown;
-
+        const cdPercent = 1 - (currentCooldown / cooldown);
+        console.log("cdPercent: " + cdPercent);
         const color = '255,0,0';
+        const textColor = 'white';
+
         return StyleSheet.create({
             button: {
-                position: 'absolute',
+                // position: 'absolute',
                 borderRadius: circleDims / 2,
                 width: circleDims,
                 height: circleDims,
                 alignItems: "center",
                 justifyContent: 'center',
-                backgroundColor: `rgba(${color},${cooldown/currentCooldown})`, // color, opacity
+                backgroundColor: `rgba(${color},${cdPercent})`, // color, opacity
             },
             text: {
-                color: 'white',
+                color: textColor,
             }
         });
     }
     updateCooldowns() {
-        for (let i = 0; i < this.cooldowns.length; i++) {
-            this.cooldowns[i].currentCooldown -= 0.1;
-            if (this.cooldowns[i].currentCooldown <= 0) {
-                // remove ability from this.cooldowns
-                i--;
-            }
+        console.log("updating cooldowns");
+        let count = 0;
+        for (let i = 0; i < this.abilitiesArr.length; i++) {
+            if (this.abilitiesArr[i].currentCooldown <= 0) continue;
+            this.abilitiesArr[i].currentCooldown -= 1;
+            count++;
         }
-        if (this.cooldowns.length == 0) {
+        if (!count) {
             clearInterval(this.timer);
             this.timer = null;
         }
+        this.setState({ state: this.state });
     }
     trackCooldowns() {
+        console.log("beginning cooldown intervel");
         this.timer = setInterval(() => {
             this.updateCooldowns();
         }, 100)
     }
     addCooldown(ability) {
+        if (ability.currentCooldown != 0) return;
+        console.log("ability added: " + JSON.stringify(ability));
         ability.currentCooldown = ability.cooldown;
         if (!this.timer) {
             this.trackCooldowns();
         }
-        this.cooldowns.push(ability);
+        // activate ability
+    }
+    componentWillUnmount() {
+        clearInterval(this.timer);
+        this.timer = null;
     }
     render() {
-        const styles = this.createStyle({ cooldown: 10 });
-        
+        let key = 0;
+        console.log("abilitiesArr: " + JSON.stringify(this.abilitiesArr));
         return (
             <View>
-                {
-                    this.abilities.map((ability) => {
-                        return (
-                            <TouchableOpacity onPress={() => this.addCooldown(ability)} style={styles.button}>
-                                <Text style={styles.text} >Click Me</Text>
-                            </TouchableOpacity>
-                        )
-                    })
-                }
+                {this.abilitiesArr.map((ability) => {
+                    // console.log("ability: " + JSON.stringify(ability));
+                    const styles = this.createStyle(ability);
+                    return (
+                        <TouchableOpacity key={key++} onPress={() => this.addCooldown(ability)} style={styles.button}>
+                            <Text style={styles.text} >Click Me</Text>
+                        </TouchableOpacity>
+                    )
+                })}
             </View>
         )
     }
