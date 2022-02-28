7ae18d77c76f651fa84334cde8f9a8197d0659ed
Jim-Heftypants <nickcsercel@gmail.com>
2022-02-28 05:03:31 -0800

    firebase data abstracted

--- Utils/firebaseFirestoreUtils.js
+++ Utils/firebaseFirestoreUtils.js
@@ -1,19 +1,6 @@
-import { initializeApp } from "firebase/app";
-// import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 import { doc, setDoc, Timestamp, getFirestore, collection, getDocs } from "firebase/firestore";
+import app from '../firebaseApp';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAxckbUXp7vOMKEh7LSfbFU_Q4fhRcr4QI",
-  authDomain: "reactnativegame-8a795.firebaseapp.com",
-  databaseURL: "https://reactnativegame-8a795-default-rtdb.firebaseio.com",
-  projectId: "reactnativegame-8a795",
-  storageBucket: "reactnativegame-8a795.appspot.com",
-  messagingSenderId: "119512401049",
-  appId: "1:119512401049:web:94d4602194bff0e9772408",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig, "React Native Game");
 const db = getFirestore(app);
 
 async function getDataList(name) {
---
+++ firebaseApp.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAxckbUXp7vOMKEh7LSfbFU_Q4fhRcr4QI",
+    authDomain: "reactnativegame-8a795.firebaseapp.com",
+    databaseURL: "https://reactnativegame-8a795-default-rtdb.firebaseio.com",
+    projectId: "reactnativegame-8a795",
+    storageBucket: "reactnativegame-8a795.appspot.com",
+    messagingSenderId: "119512401049",
+    appId: "1:119512401049:web:94d4602194bff0e9772408",
+  };
+  
+  // Initialize Firebase
+ export default app = initializeApp(firebaseConfig, "React Native Game");
