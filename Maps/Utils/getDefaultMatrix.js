d0de9b07b65dbdebde2d1bf65d6843b9eb11aa46
Jim-Heftypants <nickcsercel@gmail.com>
2022-03-24 03:02:43 -0700

    pathfinder updates

--- Maps/Utils/getDefaultMatrix.js
+++ Maps/Utils/getDefaultMatrix.js
@@ -1,10 +1,43 @@
 export default function getDefaultMatrix(x, y, value = false) {
+    // const matrix = [];
+    // for (let i = 0; i < x; i++) {
+    //     matrix.push([]);
+    //     for (let j = 0; j < y; j++) {
+    //         matrix[i].push(value);
+    //     }
+    // }
+    // return matrix;
+    return getHexMatrix(x, y);
+}
+
+function getHexMatrix(x, y) {
+    const min = 0;
+    const max = x * y + 1;
+    const dirs = [-1, 1, x, x + 1, -(x+1), -x];
     const matrix = [];
     for (let i = 0; i < x; i++) {
         matrix.push([]);
         for (let j = 0; j < y; j++) {
-            matrix[i].push(value);
+            const nodeID = getNodeID(i, j, x);
+            const neighbors = getNeighbors(nodeID, dirs, min, max, i);
+            matrix[i].push(neighbors);
         }
     }
     return matrix;
+}
+
+function getNodeID(i, j, x) {
+    return (i * x) + j;
+}
+
+function getNeighbors(nodeID, dirs, min, max, len) {
+    const neighbors = [];
+    for (let i = 0; i < dirs.length; i++) {
+        if (nodeID + dirs[i] === len) continue;
+        // if (nodeID + dirs[i] === ) continue;
+        if (nodeID + dirs[i] > min || nodeID + dirs[i] < max) {
+            neighbors.push(nodeID + dirs[i]);
+        }
+    }
+    return neighbors;
 }
--- Maps/Utils/matrixToGraph.js
+++ Maps/Utils/matrixToGraph.js
@@ -3,15 +3,16 @@ export default function matrixToGraph(matrix, size = 50) {
     const graph = {};
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
+            const nodeID = getNodeID(i, j, matrix.length);
             const center = getCenter(i, j, size);
-            const neighbors = getneighbors(matrix, i, j, size);
-            graph[center] = {
+            const neighbors = matrix[i][j];
+            // const neighbors = getneighbors(matrix, i, j, size);
+            graph[nodeID] = {
+                nodeID,
                 center,
-                neighbors, // adjacent
-                unpathable: matrix[i][j],
+                neighbors,
                 size,
                 pathableNeighbors: {},
-                // color: matrix[i][j] ? "red" : "green"
             };
         }
     }
@@ -19,22 +20,26 @@ export default function matrixToGraph(matrix, size = 50) {
     return graph;
 }
 
+function getNodeID(i, j, x) {
+    return (i * x) + j;
+}
+
 function getCenter(i, j, size) {
     return [i * size + size / 2, j * size + size / 2];
 }
 
-function getneighbors(matrix, i, j, size = 1) {
-    const neighbors = {};
-    if (i > 0) neighbors.left = (getCenter(i-1, j, size));
-    if (i > 0 && j > 0) neighbors.bottomLeft = (getCenter(i - 1, j - 1, size));
-    if (j > 0) neighbors.bottom = (getCenter(i, j - 1, size));
-    if (matrix.length - 1 > i) neighbors.right = (getCenter(i + 1, j, size));
-    if (matrix.length - 1 > i && matrix[0].length - 1 > j) neighbors.topRight = (getCenter(i + 1, j + 1, size));
-    if (matrix[0].length - 1 > j) neighbors.top = (getCenter(i, j + 1, size));
-    if (matrix.length - 1 > i && j > 0) neighbors.bottomRight = (getCenter(i + 1, j - 1, size));
-    if (matrix[0].length - 1 > j && i > 0) neighbors.topLeft = (getCenter(i - 1, j + 1, size));
-    return neighbors;
-}
+// function getneighbors(matrix, i, j, size = 1) {
+//     const neighbors = {};
+//     if (i > 0) neighbors.left = (getCenter(i-1, j, size));
+//     if (i > 0 && j > 0) neighbors.bottomLeft = (getCenter(i - 1, j - 1, size));
+//     if (j > 0) neighbors.bottom = (getCenter(i, j - 1, size));
+//     if (matrix.length - 1 > i) neighbors.right = (getCenter(i + 1, j, size));
+//     if (matrix.length - 1 > i && matrix[0].length - 1 > j) neighbors.topRight = (getCenter(i + 1, j + 1, size));
+//     if (matrix[0].length - 1 > j) neighbors.top = (getCenter(i, j + 1, size));
+//     if (matrix.length - 1 > i && j > 0) neighbors.bottomRight = (getCenter(i + 1, j - 1, size));
+//     if (matrix[0].length - 1 > j && i > 0) neighbors.topLeft = (getCenter(i - 1, j + 1, size));
+//     return neighbors;
+// }
 
 // function getneighbors(matrix, i, j, size = 1) {
 //     const neighbors = [];
