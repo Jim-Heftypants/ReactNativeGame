894f60ce2afd70688bc7bb78c4c9009da3ada6c9
Jim-Heftypants <nickcsercel@gmail.com>
2022-04-15 03:21:50 -0700

    updates

--- Maps/Utils/getDefaultMatrix.js
+++ Maps/Utils/getDefaultMatrix.js
@@ -1,13 +1,5 @@
 export default function getDefaultMatrix(x, y, value = false) {
     return getHexMatrix(x, y);
-    // const matrix = [];
-    // for (let i = 0; i < x; i++) {
-    //     matrix.push([]);
-    //     for (let j = 0; j < y; j++) {
-    //         matrix[i].push(value);
-    //     }
-    // }
-    // return matrix;
 }
 
 function getHexMatrix(x, y) {
